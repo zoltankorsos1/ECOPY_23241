3d03118159ba81515208893b743471835362643f
zoltankorsos1 <JR58KH@hallgato.nje.hu>
2023-10-24 16:04:08 +0200

    Javítás_WeeklyTest5

--- src/utils/distributions.py
+++ src/utils/distributions.py
@@ -1,103 +1,155 @@
 import random
-class NormalDistribution:
-    def __init__(self, rand, loc, scale):
-        self.rand = rand  # Véletlenszám generátor
-        self.loc = loc    # Várható érték (location)
-        self.scale = scale  # Szórás (scale)
-
 class UniformDistribution:
-    pass
+    def __init__(self, rand, a, b):
+        self.rand = rand
+        self.a = a
+        self.b = b
+
+    def pdf(self, x):
+        if self.a <= x <= self.b:
+            return 1 / (self.b - self.a)
+        else:
+            return 0.0
+
+    def cdf(self, x):
+        if x < self.a:
+            return 0.0
+        elif x >= self.b:
+            return 1.0
+        else:
+            return (x - self.a) / (self.b - self.a)
+    def ppf(self, p):
+        if 0 <= p <= 1:
+            return self.a + p * (self.b - self.a)
+        else:
+            raise ValueError("x must be between 0 and 1")
+
+    def gen_rand(self):
+        return self.rand.uniform(self.a,self.b)
+    def mean(self):
+        return (self.a + self.b) / 2
+    def median(self):
+        return (self.a + self.b) / 2
+    def variance(self):
+        import math
+        if self.a == self.b:
+            raise Exception ("Moment undefined")
+        return (math.pow((self.b - self.a), 2)) / 12
+    def skewness(self):
+        import math
+        std_dev = math.sqrt(self.variance())
+        return 3*(self.mean()-self.median())/std_dev
+    def ex_kurtosis(self):
+        return -6 / 5
+    def mvsk(self):
+        mean = self.mean()
+        variance = self.variance()
+        skewness = self.skewness()
+        ex_kurtosis = self.ex_kurtosis()
+        return [mean, variance, skewness, ex_kurtosis]
 
 class CauchyDistribution:
-    pass
+    def __init__(self, rand, loc, scale):
+        self.rand = rand  # Véletlenszám generátor
+        self.loc = loc  # x0 (location)
+        self.scale = scale  # gamma (scale)
 
-import random
+    def pdf(self, x):
+        # Számítjuk ki a Cauchy-eloszlás valószínűségi sűrűségfüggvényét
+        import math
+        if self.scale <= 0:
+            raise ValueError("A skála (scale) értéke pozitívnek kell lennie.")
 
-import random
-import math
+        probability_density = 1 / (math.pi * self.scale * (1 + ((x - self.loc) / self.scale) ** 2))
+        return probability_density
+
+    def cdf(self, x):
+        import math
+        # Számítjuk ki a Cauchy-eloszlás kumulatív eloszlásfüggvényét
+        if self.scale <= 0:
+            raise ValueError("A skála (scale) értéke pozitívnek kell lennie.")
+
+        cumulative_probability = 0.5 + (1 / math.pi) * math.atan((x - self.loc) / self.scale)
+        return cumulative_probability
+
+    def gen_random(self):
+        import math
+        # Generálunk egy Cauchy-eloszlású véletlen számot
+        u = self.rand.random()  # Véletlenszerű szám az (0, 1) intervallumból
+        random_number = self.loc + self.scale * math.tan(math.pi * (u - 0.5))
+        return random_number
+
+    def mean(self):
+        # Számítjuk ki az eloszlásfüggvény átlagát
+        raise Exception("Moment undefined")
+
+    def median(self):
+        # Az eloszlásfüggvény mediánja a lokáció értéke
+        return self.loc
 
 
 class LogisticDistribution:
-    def __init__(self, rand, location, scale):
-        self.rand = rand
-        self.location = location
-        self.scale = scale
-
-    def generate_random(self):
-        import math
-        u = self.rand.random()  # Véletlenszerű szám az [0, 1) tartományból
-        x = self.location + self.scale * math.log(u / (1 - u))  # Logistic eloszlás generálása
-        return x
-
-
-
-
-    def pdf(self, x):
-        exponent = -(x - self.location) / self.scale
-        pdf_value =math.exp(exponent) / (self.scale * (1 + math.exp(exponent))**2)
-        return pdf_value
-
-
-    def cdf(self, x):
-        exponent = math.exp(-(x - self.location) / self.scale)
-        cdf_value = 1 / (1 + exponent)
-        return cdf_value
-
-
-    def ppf(self, p):
-        import math
-        if p < 0 or p > 1:
-            raise ValueError("A valószínűségi értéknek az [0, 1] tartományban kell lennie.")
-        if p == 0:
-            return float("-inf")
-        if p == 1:
-            return float("inf")
-        ppf_value = self.location + self.scale * math.log(p / (1 - p))
-        return ppf_value
-
-    def gen_rand(self):
-        return self.generate_random()
-
-
-    def mean(self):
-        if self.scale != 0:
+        def __init__(self, rand, location, scale):
+            self.rand = rand
+            self.location = location
+            self.scale = scale
+        def generate_random(self):
+            import math
+            u = self.rand.random()  # Véletlenszerű szám az [0, 1) tartományból
+            x = self.location + self.scale * math.log(u / (1 - u))  # Logistic eloszlás generálása
+            return x
+        def pdf(self, x):
+            import math
+            exponent = -(x - self.location) / self.scale
+            pdf_value = (1 / (self.scale * (1 + math.exp(exponent)) ** 2)) * math.exp(exponent)
+            return pdf_value
+        def cdf(self, x):
+            import math
+            z = (x - self.location) / self.scale
+            cdf_value = 1 / (1 + math.exp(-z))
+            return cdf_value
+        def ppf(self, p):
+            import math
+            if p < 0 or p > 1:
+                raise ValueError("A valószínűségi értéknek az [0, 1] tartományban kell lennie.")
+            if p == 0:
+                return float("-inf")
+            if p == 1:
+                return float("inf")
+            ppf_value = self.location + self.scale * math.log(p / (1 - p))
+            return ppf_value
+        def gen_rand(self):
+            return self.generate_random()
+        def mean(self):
+            if self.scale == 0:
+                raise Exception("Moment undefined")
             return self.location
-        else:
-            raise Exception("Moment undefined")
-
-    def variance(self):
-        if self.scale != 0:
-            return (math.pi**2 * self.scale**2) / 3
-        else:
-            raise Exception("Moment undefined")
-
-
-    def skewness(self):
-        if self.scale == 0:
-            raise Exception("Moment undefined")
-        skewness = 0
-        return skewness
-
-    def ex_kurtosis(self):
-        if self.scale == 0:
-            raise Exception("Moment undefined")
-        ex_kurtosis = 1.2
-        return ex_kurtosis
-
-
-    def mvsk(self):
-        if self.scale == 0:
-            raise Exception("Moment undefined")
-        return [self.mean(), self.variance(), self.skewness(), self.ex_kurtosis()]
-
-
+        def variance(self):
+            import math
+            if self.scale == 0:
+                raise Exception("Moment undefined")
+            variance = (math.pi ** 2) * (self.scale ** 2) / 3
+            return variance
+        def skewness(self):
+            if self.scale == 0:
+                raise Exception("Moment undefined")
+            skewness = 0
+            return skewness
+        def ex_kurtosis(self):
+            if self.scale == 0:
+                raise Exception("Moment undefined")
+            ex_kurtosis = 1.2
+            return ex_kurtosis
+        def mvsk(self):
+            if self.scale == 0:
+                raise Exception("Moment undefined")
+            return [self.mean(), self.variance(), self.skewness(), self.ex_kurtosis()]
 
 
 class ChiSquaredDistribution:
     def __init__(self, rand, dof):
         self.rand = rand
         self.dof = dof
-
     def pdf(self, x):
         import math
         if x < 0:
@@ -105,14 +157,11 @@ class ChiSquaredDistribution:
         coefficient = (1 / (2**(self.dof / 2))) * (1 / math.gamma(self.dof / 2))
         exponent = (-x / 2)
         return coefficient * (x**(self.dof / 2 - 1)) * math.exp(exponent)
-
     def cdf(self, x):
         import scipy.special
         if x < 0:
             return 0
         return scipy.special.gammainc(self.dof / 2, x / 2)
-
-
     def ppf(self, p):
         import scipy.special
         if p < 0 or p > 1:
@@ -126,13 +175,10 @@ class ChiSquaredDistribution:
     def mean(self):
         try: return self.dof
         except: raise ValueError("Moment undefined")
-
     def variance(self):
         try: return 2 * self.dof
         except:
             raise ValueError("Moment undefined")
-
-
     def skewness(self):
         import math
         try: return math.sqrt(8 / self.dof)
@@ -142,8 +188,6 @@ class ChiSquaredDistribution:
         try: return 12 / self.dof
         except:
             raise ValueError("Moment undefined")
-
-
     def mvsk(self):
             mean = self.mean()
             variance = self.variance()
@@ -152,7 +196,6 @@ class ChiSquaredDistribution:
             try: return [mean, variance, skewness, ex_kurtosis]
             except:
                 raise ValueError("Moment undefined")
-
 
 
 
--- src/weekly/weekly_test_5.py
+++ src/weekly/weekly_test_5.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 import pandas as pd
-import os
+import random
+
 
 def change_price_to_float(input_df):
     modified_df = input_df.copy()
@@ -19,10 +20,12 @@ def items_and_prices(input_df):
 
     return result_df
 
-def items_and_prices(input_df):
-    result_df = input_df[['item_name', 'item_price']].copy()
 
-    return result_df
+def sorted_by_price(input_df):
+    items_prices_df = items_and_prices(input_df)
+    sorted_df = items_prices_df.sort_values(by='item_price', ascending=False)
+
+    return sorted_df
 
 def avg_price(input_df):
     average_price = input_df['item_price'].mean()
@@ -38,9 +41,8 @@ def unique_items_over_ten_dollars(input_df):
 
 def items_starting_with_s(input_df):
     filtered_df = input_df['item_name'][input_df['item_name'].str.startswith('S')]
-
-    return filtered_df
-
+    groups = filtered_df.drop_duplicates()
+    return groups
 
 
 def first_three_columns(input_df):
@@ -58,20 +60,20 @@ def sliced_view(input_df, columns_to_keep, column_to_filter, rows_to_keep):
     return filtered_df[columns_to_keep]
 
 
-def generate_quarters(input_df):
-    def quartile(price):
-        if price <=30:
-            return 'premium'
-        elif 20 <= price <= 29.99:
-            return 'high-cost'
-        elif 10 <= price <= 19.99:
-            return 'medium-cost'
-        else:
+def generate_quartile(input_df):
+    input_df = input_df.copy()
+
+    def quartile(pre):
+        if 30 < pre:
+            return "premium"
+        elif 20 <= pre <= 29.99:
+            return "high-cost"
+        elif 10 <= pre <= 19.99:
+            return "medium-cost"
+        elif 0 <= pre <= 9.99:
             return 'low-cost'
-
-    input_df['Quartile'] = input_df['Price'].apply(quartile)
+    input_df['Quartile'] = input_df['item_price'].apply(quartile)
     return input_df
-
 def average_price_in_quartiles(input_df):
     avg_price_in_quartiles = input_df.groupby('Quartile')['item_price'].mean()
     return avg_price_in_quartiles
@@ -81,159 +83,77 @@ def minmaxmean_price_in_quartile(input_df):
     minmaxmean_price = input_df.groupby('Quartile')['item_price'].agg(['min', 'max', 'mean'])
     return minmaxmean_price
 
-from random import seed, uniform
+
 def gen_uniform_mean_trajectories(distribution, number_of_trajectories, length_of_trajectory):
-    # Inicializáljuk a random generátort a megadott seed értékkel (42)
-    seed(42)
-
-    # Lista a kimeneti trajectóriák tárolására
-    trajectories = []
-
+    random.seed(42)
+    result = []
     for _ in range(number_of_trajectories):
-        # Kezdeti érték az adott trajectória kumulatív átlagához
-        cumulative_average = 0.0
-
-        # Trajectória létrehozása
         trajectory = []
-
+        cumulative_mean = 0.0
         for _ in range(length_of_trajectory):
-            # Véletlenszerűen generált szám uniform eloszlás alapján
-            random_value = distribution.uniform(0, 1)
+            value = distribution.gen_rand()
+            cumulative_mean += value
+            trajectory.append(cumulative_mean / (len(trajectory) + 1))
+        result.append(trajectory)
+    return result
 
-            # Kumulatív átlag frissítése
-            cumulative_average = (cumulative_average + random_value) / (len(trajectory) + 1)
-
-            # Hozzáadjuk a kumulatív átlagot a trajectóriához
-            trajectory.append(cumulative_average)
-
-        # Hozzáadjuk a trajectóriát a kimeneti listához
-        trajectories.append(trajectory)
-
-    return trajectories
-
-
-from random import seed, gauss
-
-def gen_logistic_mean_trajectories(distribution, number_of_trajectories, length_of_trajectory):
-    # Inicializáljuk a random generátort a megadott seed értékkel (42)
-    seed(42)
-
-    # Lista a kimeneti trajectóriák tárolására
-    trajectories = []
-
-    for _ in range(number_of_trajectories):
-        # Kezdeti érték az adott trajectória kumulatív átlagához
-        cumulative_average = 0.0
-
-        # Trajectória létrehozása
-        trajectory = []
-
-        for _ in range(length_of_trajectory):
-            # Véletlenszerűen generált szám Gauss-eloszlás alapján
-            random_value = distribution.gauss(1, 3.3)
-
-            cumulative_average = (cumulative_average + random_value) / (len(trajectory) + 1)
-
-            trajectory.append(cumulative_average)
-
-        # Hozzáadjuk a trajectóriát a kimeneti listához
-        trajectories.append(trajectory)
-
-    return trajectories
-
-
-from random import seed, gauss
 
 def gen_laplace_mean_trajectories(distribution, number_of_trajectories, length_of_trajectory):
-    # Inicializáljuk a random generátort a megadott seed értékkel (42)
-    seed(42)
-
-    # Lista a kimeneti trajectóriák tárolására
-    trajectories = []
-
+    random.seed(42)
+    result = []
     for _ in range(number_of_trajectories):
-        # Kezdeti érték az adott trajectória kumulatív átlagához
-        cumulative_average = 0.0
-
-        # Trajectória létrehozása
         trajectory = []
-
+        cumulative_mean = 0.0
         for _ in range(length_of_trajectory):
-            # Véletlenszerűen generált szám Gauss-eloszlás alapján
-            random_value = distribution.gauss(1, 3.3)
+            value = distribution.gen_rand()
+            cumulative_mean += value
+            trajectory.append(cumulative_mean / (len(trajectory) + 1))
+        result.append(trajectory)
+    return result
 
-            # Kumulatív átlag frissítése
-            cumulative_average = (cumulative_average + random_value) / (len(trajectory) + 1)
-
-            # Hozzáadjuk a kumulatív átlagot a trajectóriához
-            trajectory.append(cumulative_average)
-
-        # Hozzáadjuk a trajectóriát a kimeneti listához
-        trajectories.append(trajectory)
-
-    return trajectories
-
-from random import seed, cauchy
 
 def gen_cauchy_mean_trajectories(distribution, number_of_trajectories, length_of_trajectory):
-    # Inicializáljuk a random generátort a megadott seed értékkel (42)
-    seed(42)
-
-    # Lista a kimeneti trajectóriák tárolására
-    trajectories = []
-
+    random.seed(42)
+    result = []
     for _ in range(number_of_trajectories):
-        # Kezdeti érték az adott trajectória kumulatív átlagához
-        cumulative_average = 0.0
-
-        # Trajectória létrehozása
         trajectory = []
-
+        cumulative_mean = 0.0
         for _ in range(length_of_trajectory):
-            # Véletlenszerűen generált szám Cauchy-eloszlás alapján
-            random_value = distribution(2, 4)
+            value = distribution.gen_random()
+            cumulative_mean += value
+            trajectory.append(cumulative_mean / (len(trajectory) + 1))
+        result.append(trajectory)
+    return result
 
-            # Kumulatív átlag frissítése
-            cumulative_average = (cumulative_average + random_value) / (len(trajectory) + 1)
 
-            # Hozzáadjuk a kumulatív átlagot a trajectóriához
-            trajectory.append(cumulative_average)
+def gen_logistic_mean_trajectories(distribution, number_of_trajectories, length_of_trajectory):
+    random.seed(42)
+    result = []
+    for _ in range(number_of_trajectories):
+        trajectory = []
+        cumulative_mean = 0.0
+        for _ in range(length_of_trajectory):
+            value = distribution.generate_random()
+            cumulative_mean += value
+            trajectory.append(cumulative_mean / (len(trajectory) + 1))
+        result.append(trajectory)
+    return result
 
-        # Hozzáadjuk a trajectóriát a kimeneti listához
-        trajectories.append(trajectory)
-
-    return trajectories
-
-from random import seed, chisquare
 
 def gen_chi2_mean_trajectories(distribution, number_of_trajectories, length_of_trajectory):
-    # Inicializáljuk a random generátort a megadott seed értékkel (42)
-    seed(42)
-
-    # Lista a kimeneti trajectóriák tárolására
-    trajectories = []
-
+    random.seed(42)
+    result = []
     for _ in range(number_of_trajectories):
-        # Kezdeti érték az adott trajectória kumulatív átlagához
-        cumulative_average = 0.0
-
-        # Trajectória létrehozása
         trajectory = []
-
+        cumulative_mean = 0.0
         for _ in range(length_of_trajectory):
-            # Véletlenszerűen generált szám Chi-squared eloszlás alapján
-            random_value = distribution(3)
+            value = distribution.gen_rand()
+            cumulative_mean += value
+            trajectory.append(cumulative_mean / (len(trajectory) + 1))
+        result.append(trajectory)
+    return result
 
-            # Kumulatív átlag frissítése
-            cumulative_average = (cumulative_average + random_value) / (len(trajectory) + 1)
 
-            # Hozzáadjuk a kumulatív átlagot a trajectóriához
-            trajectory.append(cumulative_average)
-
-        # Hozzáadjuk a trajectóriát a kimeneti listához
-        trajectories.append(trajectory)
-
-    return trajectories
 
 
 
