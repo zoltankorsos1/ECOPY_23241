ebdf58d2cb6c1f619d80121761aac8c3fc5c148f
zoltankorsos1 <JR58KH@hallgato.nje.hu>
2023-11-10 15:08:50 +0100

    Javítva: összes

--- src/linear_regression/LinearRegressions.py
+++ src/linear_regression/LinearRegressions.py
@@ -1,7 +1,10 @@
-
-import pandas as pd
-from statsmodels.formula.api import ols
 import statsmodels.api as sm
+import pandas as pd
+import numpy as np
+from scipy.stats import t
+from scipy.stats import f
+
+
 class LinearRegressionSM:
     def __init__(self, left_hand_side, right_hand_side):
         self.left_hand_side = left_hand_side
@@ -35,55 +38,68 @@ class LinearRegressionSM:
         result_text = f"Adjusted R-squared: {ars:.3f}, Akaike IC: {ak:.2e}, Bayes IC: {by:.2e}"
         return result_text
 
-import pandas as pd
-import numpy as np
-from scipy.stats import t, f
-from typing import List
-
-import numpy as np
-import pandas as pd
-import statsmodels.api as sm
 
 class LinearRegressionNP:
-
     def __init__(self, left_hand_side, right_hand_side):
         self.left_hand_side = left_hand_side
         self.right_hand_side = right_hand_side
         self._model = None
-        self.coefficients = None
 
     def fit(self):
-        # Using statsmodels for OLS regression
-        X = sm.add_constant(self.right_hand_side)
-        model = sm.OLS(self.left_hand_side, X).fit()
-        self._model = model
-        self.coefficients = model.params
+        X = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
+        y = self.left_hand_side
+        beta = np.linalg.inv(X.T @ X) @ X.T @ y
+        self.beta = beta
 
-    def get_params(self) -> pd.Series:
-        if self.coefficients is not None:
-            param_names = self.coefficients.index.tolist()
-            beta_params = pd.Series(self.coefficients.values, index=param_names, name="Beta coefficients")
-            return beta_params
-        else:
-            raise ValueError("Model coefficients are not available. Fit the model first.")
-
+    def get_params(self):
+        return pd.Series(self.beta, name='Beta coefficients')
 
     def get_pvalues(self):
-        p_values = self._model.pvalues.rename("P-values for the corresponding coefficients")
+        X = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
+        y = self.left_hand_side
+        n, k = X.shape
+        beta = self.beta
+        H = X @ np.linalg.inv(X.T @ X) @ X.T
+        residuals = y - X @ beta
+        residual_variance = (residuals @ residuals) / (n - k)
+        standard_errors = np.sqrt(np.diagonal(residual_variance * np.linalg.inv(X.T @ X)))
+        t_statistics = beta / standard_errors
+        df = n - k
+        p_values = [2 * (1 - t.cdf(abs(t_stat), df)) for t_stat in t_statistics]
+        p_values = pd.Series(p_values, name="P-values for the corresponding coefficients")
         return p_values
 
-    def get_wald_test_result(self, restriction_matrix):
-        wald_result = self._model.wald_test(restriction_matrix)
-        wald_value = round(wald_result.statistic[0, 0], 3)
-        p_value = wald_result.pvalue
-        result_text = f"Wald: {wald_value:.3f}, p-value: {p_value:.3f}"
-        return result_text
+    def get_wald_test_result(self, R):
+        X = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
+        y = self.left_hand_side
+        beta = self.beta
+        residuals = y - X @ beta
+        r_matrix = np.array(R)
+        r = r_matrix @ beta
+        n = len(self.left_hand_side)
+        m, k = r_matrix.shape
+        sigma_squared = np.sum(residuals ** 2) / (n - k)
+        H = r_matrix @ np.linalg.inv(X.T @ X) @ r_matrix.T
+        wald = (r.T @ np.linalg.inv(H) @ r) / (m * sigma_squared)
+        p_value = 1 - f.cdf(wald, dfn=m, dfd=n - k)
+        return f'Wald: {wald:.3f}, p-value: {p_value:.3f}'
 
     def get_model_goodness_values(self):
-        centered_r_squared = self._model.rsquared
-        adjusted_r_squared = self._model.rsquared_adj
-        result_text = f"Centered R-squared: {centered_r_squared:.3f}, Adjusted R-squared: {adjusted_r_squared:.3f}"
-        return result_text
+        X = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
+        y = self.left_hand_side
+        n, k = X.shape
+        beta = self.beta
+        y_pred = X @ beta
+        ssr = np.sum((y_pred - np.mean(y)) ** 2)
+        sst = np.sum((y - np.mean(y)) ** 2)
+        centered_r_squared = ssr / sst
+        adjusted_r_squared = 1 - (1 - centered_r_squared) * (n - 1) / (n - k)
+        result = f"Centered R-squared: {centered_r_squared:.3f}, Adjusted R-squared: {adjusted_r_squared:.3f}"
+        return result
+
+
+
+
 
 
 
