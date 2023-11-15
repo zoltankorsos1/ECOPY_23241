7c7f06121802ed0f09f2613fbf9765f6e9a2230b
zoltankorsos1 <JR58KH@hallgato.nje.hu>
2023-11-15 20:22:12 +0100

    JR58KH

--- src/linear_regression/LinearRegressions.py
+++ src/linear_regression/LinearRegressions.py
@@ -99,19 +99,60 @@ class LinearRegressionNP:
 
 
 
+class LinearRegressionGLS:
+    def __init__(self, left_hand_side, right_hand_side):
+        self.left_hand_side = left_hand_side
+        self.right_hand_side = right_hand_side
+        self.beta = None  # Change to self.beta for consistency
 
 
+    def fit(self):
+        X = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
+        y = self.left_hand_side
+        beta = np.linalg.inv(X.T @ X) @ X.T @ y
+        self.beta = beta
+
+    def get_params(self):
+        return pd.Series(self.beta, name='Beta coefficients')
+
+    def get_pvalues(self):
+        if self.beta_coefficients is None:
+            raise ValueError("A modell még nem lett becslve. Kérlek futtasd el a fit metódust először.")
+
+        # Compute t-statistic and p-values
+        X = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
+        residuals = self.left_hand_side - X @ self.beta_coefficients
+        squared_residuals = residuals**2
+        X_squared = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side, squared_residuals))
+        t_statistic = self.beta_coefficients / np.sqrt(np.diag(np.linalg.inv(X_squared.T @ X_squared)))
+        p_values = pd.Series(np.minimum(t_statistic, 1 - t_statistic) * 2, index=range(len(self.beta_coefficients)))
+        p_values.name = 'P-values for the corresponding coefficients'
+        return p_values
+
+    def get_wald_test_result(self, R):
+        if self.beta_coefficients is None:
+            raise ValueError("A modell még nem lett becslve. Kérlek futtasd el a fit metódust először.")
+        wald_value = float((R @ self.beta_coefficients).T @ np.linalg.inv(R @ np.linalg.inv(X.T @ weights @ X) @ R.T) @ (R @ self.beta_coefficients))
+        p_value = 1 - stats.chi2.cdf(wald_value, len(R))
+        result_string = f"Wald: {wald_value:.3f}, p-value: {p_value:.3f}"
+        return result_string
 
 
+    def get_model_goodness_values(self):
+        if self.beta_coefficients is None:
+            raise ValueError("A modell még nem lett becslve. Kérlek futtasd el a fit metódust először.")
+        residuals = self.left_hand_side - X @ self.beta_coefficients.values
+        SSR = np.sum(residuals**2)
+        mean_response = np.mean(self.left_hand_side)
+        SST = np.sum((self.left_hand_side - mean_response)**2)
 
+        crs = 1 - SSR / SST
+        n = len(self.left_hand_side)
+        k = X.shape[1]  # A változók száma (beleértve a konstanst is)
+        ars = 1 - (1 - crs) * (n - 1) / (n - k - 1)
 
+        result_string = f"Centered R-squared: {crs:.3f}, Adjusted R-squared: {ars:.3f}"
 
-
-
-
-
-
-
-
+        return result_string
 
 
