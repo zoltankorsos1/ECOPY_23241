682b37099d2f714105ecf80443881228d6fb3f56
zoltankorsos1 <JR58KH@hallgato.nje.hu>
2023-11-21 16:36:03 +0100

    JR58KH_GLS_Javítás

--- src/linear_regression/LinearRegressions.py
+++ src/linear_regression/LinearRegressions.py
@@ -98,61 +98,95 @@ class LinearRegressionNP:
         return result
 
 
+import pandas as pd
+import numpy as np
+from scipy.stats import t, f as f_dist
 
 class LinearRegressionGLS:
     def __init__(self, left_hand_side, right_hand_side):
-        self.left_hand_side = left_hand_side
-        self.right_hand_side = right_hand_side
-        self.beta = None  # Change to self.beta for consistency
-
+        # Initialize the class with response and predictor variables
+        self.response_var = left_hand_side.values
+        self.predictor_var = right_hand_side.values
 
     def fit(self):
-        X = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
-        y = self.left_hand_side
-        beta = np.linalg.inv(X.T @ X) @ X.T @ y
-        self.beta = beta
+        # Fit the Generalized Least Squares model
+        self.design_matrix = np.column_stack((np.ones(len(self.predictor_var)), self.predictor_var))
+        self.observed_values = self.response_var
+
+        # Calculate initial OLS estimates
+        beta_initial = np.linalg.inv(self.design_matrix.T @ self.design_matrix) @ self.design_matrix.T @ self.observed_values
+        residuals_initial = self.observed_values - self.design_matrix @ beta_initial
+        log_resid_initial = np.log(residuals_initial ** 2)
+
+        # Weighted OLS
+        beta_weighted = np.linalg.inv(self.design_matrix.T @ self.design_matrix) @ self.design_matrix.T @ log_resid_initial
+        weight_diag = 1 / np.sqrt(np.exp(self.design_matrix @ beta_weighted))
+        self.weights_inv = np.diag(weight_diag)
+
+        # GLS estimates
+        beta_gls = np.linalg.inv(self.design_matrix.T @ self.weights_inv @ self.design_matrix) @ self.design_matrix.T @ self.weights_inv @ self.observed_values
+        self.coefficients = beta_gls
 
     def get_params(self):
-        return pd.Series(self.beta, name='Beta coefficients')
+        # Get model parameters
+        return pd.Series(self.coefficients, name='Beta coefficients')
 
     def get_pvalues(self):
-        if self.beta_coefficients is None:
-            raise ValueError("A modell még nem lett becslve. Kérlek futtasd el a fit metódust először.")
+        # Calculate p-values for coefficients
+        self.fit()
+        degrees_of_freedom = len(self.observed_values) - self.design_matrix.shape[1]
+        residuals = self.observed_values - self.design_matrix @ self.coefficients
+        residual_var = (residuals @ residuals) / degrees_of_freedom
+        t_stat = self.coefficients / np.sqrt(np.diag(residual_var * np.linalg.inv(self.design_matrix.T @ self.weights_inv @ self.design_matrix)))
 
-        # Compute t-statistic and p-values
-        X = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
-        residuals = self.left_hand_side - X @ self.beta_coefficients
-        squared_residuals = residuals**2
-        X_squared = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side, squared_residuals))
-        t_statistic = self.beta_coefficients / np.sqrt(np.diag(np.linalg.inv(X_squared.T @ X_squared)))
-        p_values = pd.Series(np.minimum(t_statistic, 1 - t_statistic) * 2, index=range(len(self.beta_coefficients)))
-        p_values.name = 'P-values for the corresponding coefficients'
+        # Calculate two-tailed p-values using the t distribution
+        p_values = pd.Series([min(value, 1 - value) * 2 for value in t.cdf(-np.abs(t_stat), df=degrees_of_freedom)],
+                             name='P-values for the corresponding coefficients')
         return p_values
 
-    def get_wald_test_result(self, R):
-        if self.beta_coefficients is None:
-            raise ValueError("A modell még nem lett becslve. Kérlek futtasd el a fit metódust először.")
-        wald_value = float((R @ self.beta_coefficients).T @ np.linalg.inv(R @ np.linalg.inv(X.T @ weights @ X) @ R.T) @ (R @ self.beta_coefficients))
-        p_value = 1 - stats.chi2.cdf(wald_value, len(R))
-        result_string = f"Wald: {wald_value:.3f}, p-value: {p_value:.3f}"
-        return result_string
+    def get_wald_test_result(self, constraints_matrix):
+        # Perform Wald test
+        self.fit()
+        r_matrix = np.array(constraints_matrix)
+        r_transformed = r_matrix @ self.coefficients
+        n_obs = len(self.observed_values)
+        m_constraints, k_predictors = r_matrix.shape
+        residuals = self.observed_values - self.design_matrix @ self.coefficients
+        residual_var = (residuals @ residuals) / (n_obs - k_predictors)
+        h_matrix = r_matrix @ np.linalg.inv(self.design_matrix.T @ self.weights_inv @ self.design_matrix) @ r_matrix.T
+        wald_statistic = (r_transformed.T @ np.linalg.inv(h_matrix) @ r_transformed) / (m_constraints * residual_var)
 
+        # Calculate p-value for the Wald statistic using the F distribution
+        p_value = 1 - f_dist.cdf(wald_statistic, dfn=m_constraints, dfd=n_obs - k_predictors)
+        return f'Wald: {wald_statistic:.3f}, p-value: {p_value:.3f}'
 
     def get_model_goodness_values(self):
-        if self.beta_coefficients is None:
-            raise ValueError("A modell még nem lett becslve. Kérlek futtasd el a fit metódust először.")
-        residuals = self.left_hand_side - X @ self.beta_coefficients.values
-        SSR = np.sum(residuals**2)
-        mean_response = np.mean(self.left_hand_side)
-        SST = np.sum((self.left_hand_side - mean_response)**2)
+        # Calculate goodness-of-fit measures
+        self.fit()
+        total_sos = self.observed_values.T @ self.weights_inv @ self.observed_values
+        residual_sos = self.observed_values.T @ self.weights_inv @ self.design_matrix @ np.linalg.inv(self.design_matrix.T @ self.weights_inv @ self.design_matrix) @ self.design_matrix.T @ self.weights_inv @ self.observed_values
+        centered_r_squared_new = 1 - (residual_sos / total_sos)
+        adjusted_r_squared_new = 1 - (residual_sos / (len(self.observed_values) - self.design_matrix.shape[1])) * (
+                len(self.observed_values) - 1) / total_sos
 
-        crs = 1 - SSR / SST
-        n = len(self.left_hand_side)
-        k = X.shape[1]  # A változók száma (beleértve a konstanst is)
-        ars = 1 - (1 - crs) * (n - 1) / (n - k - 1)
+        return f"Centered R-squared: {centered_r_squared_new:.3f}, Adjusted R-squared: {adjusted_r_squared_new:.3f}"
 
-        result_string = f"Centered R-squared: {crs:.3f}, Adjusted R-squared: {ars:.3f}"
 
-        return result_string
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
